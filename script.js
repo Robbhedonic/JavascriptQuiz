63402513b3f8f6a8da69c08b76b937f208ef43f7
Robbhedonic <rccstockholm@outlook.com>
2023-12-07 15:13:26 +0100

    Colour result

--- script.js
+++ script.js
@@ -30,12 +30,11 @@ const questions = [
         ]
     },
     {
-        question: "Which is largest river in Africa ",
+        question: "Is America largest continent? ",
         answers:[
-            {text: "Amazonas", correct:false},
-            {text: "White River", correct:false},
-            {text: "Nilo", correct:true},
-            {text: "Cocus", correct:false},
+            {text: "False", correct:true},
+            {text: "True", correct:false},
+            
         ]
     },
     {
@@ -57,19 +56,61 @@ const questions = [
         ]
     },
     {
-        question: "Which is the smallest continent in the world?",
+        question: "Is true that deepest lake is in Russia?",
         answers:[
-            {text: "Asia", correct:false},
-            {text: "Australia", correct:true},
-            {text: "Arctic", correct:false},
-            {text: "Africa", correct:false},
+            {text: "False", correct:false},
+            {text: "True", correct:true},
+           
+        ]
+    },
+     {
+        question: "Is Tuvalu the smallest continent in the world?",
+        answers:[
+            {text: "True", correct:false},
+            {text: "False", correct:true}
+        ]
+    },
+    {
+        question: "Which is smallest country in the world?",
+        answers:[
+            {text: "Vatican city", correct:true},
+            {text: "Bhutan", correct:false},
+            {text: "Nepal", correct:false},
+            {text: "Sri Lanka", correct:false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answers:[
+            {text: "Kalahari", correct:false},
+            {text: "Gobi", correct:false},
+            {text: "Sahara", correct:false},
+            {text: "Antarctica", correct:true},
+        ]
+    },
+    {
+        question: "Is San Marino the old nation in the world?",
+        answers:[
+            {text: "It is San Marino", correct:false},
+            {text: "No, it is Vatican", correct:true},
+            {text: "Japan", correct:false},
+            {text: "Brunei", correct:false},
+        ]
+    },
+     {
+        question: "Is Tuvalu the smallest continent in the world?",
+        answers:[
+            {text: "True", correct:false},
+            {text: "False", correct:true}
         ]
     }
+  
 
 ];
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const pointsElement = document.getElementById("points");
 
 
 let currentQuestionIndex = 0;
@@ -126,13 +167,29 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 
 }
+ 
+
 
 function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    
+
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
-}
+    if(score > 7){
+        resultt.classList.add("high-score");
+        pointsElement.innerHTML = `More that 75%,Very Good!`;
+    }else if(score >5 && score< 8){
+        resultt.classList.add("medium-score");
+        pointsElement.innerHTML = `Between 51% and 75%,Good!`;
+
+    }else{resultt.classList.add("low-score");
+        pointsElement.innerHTML = `Less that 51%, No Good!`;
+    
+    }}
+    
+
 
 function handleNextButton(){
     currentQuestionIndex++;
